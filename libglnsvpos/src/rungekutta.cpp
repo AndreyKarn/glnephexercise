d4ed595e97af248835bee639c2a045173183d27d
Vlad <ZherebinVR@ya.ru>
2020-05-12 15:39:25 +0300

    Коррекция ошибок

--- libglnsvpos/src/rungekutta.cpp
+++ libglnsvpos/src/rungekutta.cpp
@@ -27,9 +27,9 @@ Y_s* diffs(double tn , struct Y_s Y)
     dY->F2 = Y.F5;
     dY->F3 = Y.F6;
 
-    dY->F4 = - GM0 * crdX0 - 3/2 * J02 * GM0 * crdX0 * Rho * Rho * (1 - 5 * crdZ0 * crdZ0);
-    dY->F5 = - GM0 * crdY0 - 3/2 * J02 * GM0 * crdY0 * Rho * Rho * (1 - 5 * crdZ0 * crdZ0);
-    dY->F6 = - GM0 * crdZ0 - 3/2 * J02 * GM0 * crdZ0 * Rho * Rho * (3 - 5 * crdZ0 * crdZ0);
+    dY->F4 = - GM0 * crdX0 - (double)1.5 * J02 * GM0 * crdX0 * Rho * Rho * (1 - (double)5 * crdZ0 * crdZ0);
+    dY->F5 = - GM0 * crdY0 - (double)1.5 * J02 * GM0 * crdY0 * Rho * Rho * (1 - (double)5 * crdZ0 * crdZ0);
+    dY->F6 = - GM0 * crdZ0 - (double)1.5 * J02 * GM0 * crdZ0 * Rho * Rho * (3 - (double)5 * crdZ0 * crdZ0);
 
     return dY;
 }
@@ -49,23 +49,25 @@ int RK(uint32_t N, double h, struct Y_s* Y) {
         k4 = new struct Y_s;
         knextstep = new struct Y_s;
 
+        double two = 2;
+
         k1 = diffs(0, Y[k-1]);
 
-        Y2.F1 = Y->F1 + h * k1->F1 / 2;
-        Y2.F2 = Y->F2 + h * k1->F2 / 2;
-        Y2.F3 = Y->F3 + h * k1->F3 / 2;
-        Y2.F4 = Y->F4 + h * k1->F4 / 2;
-        Y2.F5 = Y->F5 + h * k1->F5 / 2;
-        Y2.F6 = Y->F6 + h * k1->F6 / 2;
+        Y2.F1 = Y->F1 + h * k1->F1 / two;
+        Y2.F2 = Y->F2 + h * k1->F2 / two;
+        Y2.F3 = Y->F3 + h * k1->F3 / two;
+        Y2.F4 = Y->F4 + h * k1->F4 / two;
+        Y2.F5 = Y->F5 + h * k1->F5 / two;
+        Y2.F6 = Y->F6 + h * k1->F6 / two;
 
         k2 = diffs (0 + h / 2, Y2);
 
-        Y3.F1 = Y->F1 + h * k2->F1 / 2;
-        Y3.F2 = Y->F2 + h * k2->F2 / 2;
-        Y3.F3 = Y->F3 + h * k2->F3 / 2;
-        Y3.F4 = Y->F4 + h * k2->F4 / 2;
-        Y3.F5 = Y->F5 + h * k2->F5 / 2;
-        Y3.F6 = Y->F6 + h * k2->F6 / 2;
+        Y3.F1 = Y->F1 + h * k2->F1 / two;
+        Y3.F2 = Y->F2 + h * k2->F2 / two;
+        Y3.F3 = Y->F3 + h * k2->F3 / two;
+        Y3.F4 = Y->F4 + h * k2->F4 / two;
+        Y3.F5 = Y->F5 + h * k2->F5 / two;
+        Y3.F6 = Y->F6 + h * k2->F6 / two;
 
         k3 = diffs (0 + h / 2 , Y3);
 
@@ -78,12 +80,14 @@ int RK(uint32_t N, double h, struct Y_s* Y) {
 
         k4 = diffs (0 + h , Y4);
 
-        knextstep->F1 = h / 6 * ( k1->F1 + 2 * k2->F1 + 2 * k3->F1 + k4->F1 );
-        knextstep->F2 = h / 6 * ( k1->F2 + 2 * k2->F2 + 2 * k3->F2 + k4->F2 );
-        knextstep->F3 = h / 6 * ( k1->F3 + 2 * k2->F3 + 2 * k3->F3 + k4->F3 );
-        knextstep->F4 = h / 6 * ( k1->F4 + 2 * k2->F4 + 2 * k3->F4 + k4->F4 );
-        knextstep->F5 = h / 6 * ( k1->F5 + 2 * k2->F5 + 2 * k3->F5 + k4->F5 );
-        knextstep->F6 = h / 6 * ( k1->F6 + 2 * k2->F6 + 2 * k3->F6 + k4->F6 );
+        double h_6 = h / (double)6;
+
+        knextstep->F1 = h_6 * ( k1->F1 + two * k2->F1 + two * k3->F1 + k4->F1 );
+        knextstep->F2 = h_6 * ( k1->F2 + two * k2->F2 + two * k3->F2 + k4->F2 );
+        knextstep->F3 = h_6 * ( k1->F3 + two * k2->F3 + two * k3->F3 + k4->F3 );
+        knextstep->F4 = h_6 * ( k1->F4 + two * k2->F4 + two * k3->F4 + k4->F4 );
+        knextstep->F5 = h_6 * ( k1->F5 + two * k2->F5 + two * k3->F5 + k4->F5 );
+        knextstep->F6 = h_6 * ( k1->F6 + two * k2->F6 + two * k3->F6 + k4->F6 );
 
         Y[k].F1 = Y[k-1].F1 + knextstep->F1;
         Y[k].F2 = Y[k-1].F2 + knextstep->F2;
