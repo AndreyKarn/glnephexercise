731118d53f74f59094deef7b8b589159ef75329d
Vlad <ZherebinVR@ya.ru>
2020-05-21 18:45:14 +0300

    Настроен тест для функции NT_calc()

--- libglnsvpos/Boost_test/main.cpp
+++ libglnsvpos/Boost_test/main.cpp
@@ -4,7 +4,7 @@
 // **************** HOW TO START ****************
 // 1. Build this project
 // 2. Open Console
-// 3. Move to path "..\glnephexercise\libglnsvpos\build-Boost_test-Desktop_Qt_5_14_2_MinGW_64_bit-Debug\debug"
+// 3. Move to dir "..\glnephexercise\libglnsvpos\build-Boost_test-Desktop_Qt_5_14_2_MinGW_64_bit-Debug\debug"
 // 4. Launch "Boost_test.exe" with key " --log_level=test_suite --run_test=+test_net"
 // Example "D:\Repository\glnephexercise\libglnsvpos\build-Boost_test-Desktop_Qt_5_14_2_MinGW_64_bit-Debug\debug>Boost_test.exe --log_level=test_suite --run_test=+test_net"
 // **************** HOW TO START ****************
@@ -15,20 +15,49 @@
 
 namespace utf = boost::unit_test;
 
-BOOST_AUTO_TEST_CASE(test_add)
+BOOST_AUTO_TEST_CASE(test_NT_calc)
 {
-    int a = 2;
-    int b = 3;
-    int c = add(a,b);
+    // In data
+    uint16_t Time_year = 2020;
+    uint8_t Time_month = 2;
+    uint8_t Time_day = 25;
 
-    BOOST_CHECK( c == a+b );
+    // expected
+    uint8_t N4exp = 7;
+    uint16_t NTexp = 57;
+
+    // actual
+    uint8_t N4act;
+    uint16_t NTact;
+
+    // Function
+    uint16_t year_idx = 0;
+    N4act = ((Time_year - 1996) / 4) + 1;
+    while (N4act > 31) { // Учет 5-битности N4
+        N4act -= 31;
+        year_idx++;
+    }
+    NTact = NT_calc(N4act, Time_year, year_idx, Time_month, Time_day);
+
+    // Result
+    BOOST_CHECK( N4exp == N4act );
+    BOOST_CHECK( NTexp == NTact );
 }
 
-BOOST_AUTO_TEST_CASE(test_mult)
-{
-    int a = 2;
-    int b = 3;
-    int c = mult(a,b);
+//BOOST_AUTO_TEST_CASE(test_add)
+//{
+//    int a = 2;
+//    int b = 3;
+//    int c = add(a,b);
 
-    BOOST_CHECK( c == a*b );
-}
+//    BOOST_CHECK( c == a+b );
+//}
+
+//BOOST_AUTO_TEST_CASE(test_mult)
+//{
+//    int a = 2;
+//    int b = 3;
+//    int c = mult(a,b);
+
+//    BOOST_CHECK( c == a*b );
+//}
