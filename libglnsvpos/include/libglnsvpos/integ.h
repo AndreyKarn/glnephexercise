78b31b7779151658447a8e3331858eb7078aedbf
KaluginKS <vorobey57ru@gmail.com>
2020-06-26 00:21:28 +0300

    integ.h

---
+++ libglnsvpos/include/libglnsvpos/integ.h
@@ -0,0 +1,10 @@
+#include<tuple>
+using namespace std;
+
+#ifndef INTEG_H
+#define INTEG_H
+
+tuple<double,double,double,double,double,double> integ(double *f);
+//void integ(double *f, double *dif);
+
+#endif // INTEG_H
